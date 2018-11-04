727bba37334026539378cabb23a83aa44ce45ca0
thawdahtun <reporterlayhlaing@gmail.com>
2018-11-04 18:28:50 +0630

    My First Commit

---
+++ app/view/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home Page</title>
+</head>
+<body>
+	<h1>Home Page</h1>
+	<h3>Hello World!</h3>
+	<p>Welcome for Home page.</p>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+if (isset($_GET['page'])) {
+	$page = htmlspecialchars($_GET['page']);
+	$file = "../app/view/".$page.".php";
+	if (file_exists($file)) {
+		require $file;
+	} else {
+		require "../app/view/404.php";
+	}
+} else {
+	require "../app/view/index.php";
+}
+
+ ?>
